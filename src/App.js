2f2c9fa859020c5c73ce5a31808e1f06994d49e9
Mahima <mahima011.jain@gmail.com>
2021-08-09 01:42:16 +0530

    adding files

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import Game from './components/Game';
+import './index.css'
+
+function App() {
+  return (
+   <Game/>
+  );
+}
+
+export default App;
